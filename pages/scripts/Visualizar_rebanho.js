9e166f2a05209667764aeb986d4e80233d9bde34
Danieu1 <fjuliano546@gmail.com>
2023-12-13 23:18:01 -0300

    quebra de cards

--- pages/scripts/Visualizar_rebanho.js
+++ pages/scripts/Visualizar_rebanho.js
@@ -3,20 +3,27 @@ window.onload = () => {
     let usuario =  JSON.parse(localStorage.getItem('usuarioLogado'));
     let animaisSection = document.getElementById("rebanho");
 
-    for(let i=0; i < animais.length;i++){
-        if(usuario.email === animais[i].email_animal){
+    // Adiciona uma classe "animal-grid" ao contêiner
+    animaisSection.classList.add("animal-grid");
+    
+    for (let i = 0; i < animais.length; i++) {
+        if (usuario.email === animais[i].email_animal) {
             let sectionAnimal = document.createElement("section");
             sectionAnimal.setAttribute("id", animais[i].id_colar);
             sectionAnimal.setAttribute("class", "animal");
+    
             let idAnimal = document.createElement("p");
-            idAnimal.innerHTML = `ID: ${animais[i].id_colar}`;    
+            idAnimal.innerHTML = `ID: ${animais[i].id_colar}`;
             idAnimal.setAttribute("class", "idAnimal");
+    
             let especieAnimal = document.createElement("p");
-            especieAnimal.innerHTML = `Espécie: ${animais[i].especie}`;    
+            especieAnimal.innerHTML = `Espécie: ${animais[i].especie}`;
             especieAnimal.setAttribute("class", "especieAnimal");
+    
             sectionAnimal.appendChild(idAnimal);
             sectionAnimal.appendChild(especieAnimal);
-            sectionAnimal.addEventListener("click",describeAnimal, );
+            sectionAnimal.addEventListener("click", describeAnimal);
+    
             animaisSection.appendChild(sectionAnimal);
         }
     }
